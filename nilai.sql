b585c19653cb8863c69c6c408090f1f5470b5f7b
Muhammad Zulfi Izzulhaq <zulfi.izzulhaq@gmail.com>
2019-01-05 09:28:03 +0700

    database

--- nilai.sql
+++ nilai.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Dec 08, 2018 at 11:36 PM
+-- Generation Time: Jan 05, 2019 at 03:19 AM
 -- Server version: 10.1.35-MariaDB
 -- PHP Version: 7.2.9
 
